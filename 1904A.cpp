e2cd3773de4ade2007da6d443f4676a2d7c8b962
priyachau12 <125198162+priyachau12@users.noreply.github.com>
2025-06-12 23:57:54 +0530

    Add 1904A.cpp

---
+++ 1904A.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        ll a,b,xk,yk,xq,yq;
+        vector<pair<ll,ll>>vec1(8);
+        vector<pair<ll,ll>>vec2(8);          
+        cin>>a>>b>>xk>>yk>>xq>>yq;
+        vec1[0] = make_pair(xk + a , yk + b);//1
+        vec1[0] = make_pair(xk + a , yk + b);//2
+        vec1[0] = make_pair(xk + a , yk + b);//3
+        vec1[0] = make_pair(xk + a , yk + b);//4
+        vec1[0] = make_pair(xk + a , yk + b);//5
+        vec1[0] = make_pair(xk + a , yk + b);//6
+        vec1[0] = make_pair(xk + a , yk + b);
+
+    }
+    
+}
