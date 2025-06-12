40841e45e940ced78fdabe31f8933f2f51403910
priyachau12 <125198162+priyachau12@users.noreply.github.com>
2025-06-12 23:59:12 +0530

    Add 1999A.cpp

---
+++ 1999A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve()
+{
+  int n;
+  cin>>n;
+  cout<<(n%10)+(n/10)<<endl;
+  return 0;  
+}
+
+
+int main()
+{
+  int tc;
+  cin>>tc;
+  while(tc--)
+  {
+    solve();
+  }
+
+}
