45f767aa1eacf107c28e0d61857db8c9c27c4fe9
priyachau12 <125198162+priyachau12@users.noreply.github.com>
2025-06-12 23:57:34 +0530

    Add 1894A.cpp

---
+++ 1894A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int solve()
+{
+    int n; cin>>n;
+    string str; cin>>str;
+    cout<<str[n-1]<<endl;
+}
+
+int main()
+{
+    int t; cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+}
