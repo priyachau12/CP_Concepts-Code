482a1f35e19604451595ed814311d13ff5c99d12
priyachau12 <125198162+priyachau12@users.noreply.github.com>
2025-06-12 23:52:33 +0530

    Add 1691B.cpp

---
+++ 1691B.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve()
+{
+  int n;
+  cin>>n;
+  vector<int>vec(n);
+  for(int i=0; i<n; i++)
+  {
+    cin>>vec[i];
+  }
+
+  int l=0, r=0;
+  while(l<n && r<n)
+  {
+    
+  }
+  cout<<endl;
+  return 0;  
+}
+/*
+0 + 4 - 4 + 1
+
+1 2 3 4 5 6 7 8 9 
+2 2 2 2 3 3 3 3 3 
+4 3 2 1 9 5 
+*/
+int main()
+{
+  int tc;
+  cin>>tc;
+  while(tc--)
+  {
+    solve();
+  }
+
+}
