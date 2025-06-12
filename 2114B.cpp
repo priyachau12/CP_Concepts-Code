5b6a82130b33dcb6e351ba5cd2f0a8274da5c5aa
priyachau12 <125198162+priyachau12@users.noreply.github.com>
2025-06-13 00:01:33 +0530

    Add 2114B.cpp

---
+++ 2114B.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+int solve()
+{
+    int n, k;
+    cin>>n>>k;
+    string s;
+    cin>>s;
+    int oc=0, zc=0;
+    for (char c:s)
+    {
+        oc += c=='1';
+        zc += c=='0';
+    }
+    0
+    return 0;
+}
+
+int32_t main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        solve();
+    }
+}
