448b49ea6ef3d5d4fe4304b44c18ccb1578df414
priyachau12 <125198162+priyachau12@users.noreply.github.com>
2025-06-12 23:53:50 +0530

    Add 1771A.cpp

---
+++ 1771A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve()
+{
+  int n, max = -1, min = 1e6;
+  cin>>n; 
+  return 0;  
+}
+
+
+int main()
+{
+  int tc;
+  cin>>tc;
+  while(tc--)
+  {
+    solve();
+  }
+
+}
