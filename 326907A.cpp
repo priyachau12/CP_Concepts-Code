1013702d627192c430b5ae96cac18e75c9f0c1a8
priyachau12 <125198162+priyachau12@users.noreply.github.com>
2025-06-13 00:02:18 +0530

    Add 326907A.cpp

---
+++ 326907A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve()
+{
+  long long a,b=0;
+  cin>>a>>b;
+  cout<<max(0ll, a-b);
+  return 0;  
+
+}
+
+
+int main()
+{
+  int tc=1;
+  //cin>>tc;
+  while(tc--)
+  {
+    solve();
+  }
+
+}
