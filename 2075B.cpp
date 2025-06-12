9603ff0b97f779f7d281eaffcd76ce53bb17df0c
priyachau12 <125198162+priyachau12@users.noreply.github.com>
2025-06-13 00:00:48 +0530

    Add 2075B.cpp

---
+++ 2075B.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+int solve()
+{
+  int n,k;
+  cin>>n>>k;
+  vector<int>vec(n);
+  for(int i=0; i<n; i++)
+  {
+    cin>>vec[i];
+  } 
+  vector<int>tmp(vec.begin(), vec.end());
+  
+  if(k==1)
+  {
+
+  }
+  if(k>1)
+  {
+    
+  }
+
+  return 0;  
+}
+
+
+int32_t main()
+{
+  int tc;
+  cin>>tc;
+  while(tc--)
+  {
+    solve();
+  }
+
+}
