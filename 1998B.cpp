54911baa421b46d220bd2d9fffb1cee616223193
priyachau12 <125198162+priyachau12@users.noreply.github.com>
2025-06-12 23:59:10 +0530

    Add 1998B.cpp

---
+++ 1998B.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve()
+{
+  int n;
+  cin>>n;
+  
+  return 0;  
+}
+
+
+int main()
+{
+  int tc;
+  cin>>tc;
+  while(tc--)
+  {
+    solve();
+  }
+
+}
