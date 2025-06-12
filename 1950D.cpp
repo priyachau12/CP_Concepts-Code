e4c85d9b277a1f27ca56c7478a311d6b69153a9b
priyachau12 <125198162+priyachau12@users.noreply.github.com>
2025-06-12 23:58:24 +0530

    Add 1950D.cpp

---
+++ 1950D.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<bool> bin(5*10e5,false);
+
+bool checkbind(int n)
+{
+    while(n>0)
+    {
+        if(n%10)
+    }
+}
+
+int precomp()
+{
+    for(int i=0;i<10e5 + 1;i++)
+    {
+        if(checkbind(i))
+        {
+            bin[i] = true;
+        }
+    }
+}
