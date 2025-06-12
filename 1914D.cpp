0c17ee79c39735c2542c8e23724820f30ed777ab
priyachau12 <125198162+priyachau12@users.noreply.github.com>
2025-06-12 23:58:05 +0530

    Add 1914D.cpp

---
+++ 1914D.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+int solve()
+{
+	int n;
+	cin>>n;
+	vector<int>a(n),b(n),c(n);
+	for( auto &i: a) cin>>i;
+	for( auto &i: b) cin>>i;
+	for( auto &i: c) cin>>i;
+
+	partial_sort(a.begin(), a.begin()+3, a.end())
+  return 0;  
+}
+
+
+int32_t main()
+{
+  int tc;
+  cin>>tc;
+  while(tc--)
+  {
+    solve();
+  }
+
+}
