677d98f39fc8c897d9ae2ded4f2b99859ef3a796
priyachau12 <125198162+priyachau12@users.noreply.github.com>
2025-06-12 11:39:53 +0530

    commited 69

---
+++ 69A.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int n;
+  cin>>n;
+  long long x=0,y=0,z=0;
+  for(int i=0;i<n;i++)
+  {
+    long long a,b,c;
+    cin>>a>>b>>c;
+    x+=a;
+    y+=b;
+    z+=c;
+  }
+  if(x==0 && y==0 && z==0)
+  {
+    cout<<"YES\n";
+  }
+  else
+  {
+    cout<<"NO\n";
+  }
+}
