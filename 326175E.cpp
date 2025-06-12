e02a8f5557096350a15629efe2f160a7fc77e421
priyachau12 <125198162+priyachau12@users.noreply.github.com>
2025-06-13 00:02:08 +0530

    Add 326175E.cpp

---
+++ 326175E.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve()
+{
+  int a,b;
+  cin>>a>>b;
+  cout<<(a^b)<<endl;
+  return 0;  
+}
+
+
+int main()
+{
+  int tc=1;
+  //cin>>tc;
+  while(tc--)
+  {
+    solve();
+  }
+
+}
