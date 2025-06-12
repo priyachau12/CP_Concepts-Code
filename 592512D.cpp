4059500b7237c280914bd2e538c38c8ddf05aee6
priyachau12 <125198162+priyachau12@users.noreply.github.com>
2025-06-13 00:03:57 +0530

    Add 592512D.cpp

---
+++ 592512D.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+int solve()
+{
+   
+  return 0;  
+}
+
+
+int32_t main()
+{
+  int tc;
+  cin>>tc;
+  while(tc--)
+  {
+    solve();
+  }
+
+}
