6583471d5602e55ae418db608fc5184307e18981
priyachau12 <125198162+priyachau12@users.noreply.github.com>
2025-06-12 23:59:34 +0530

    Add 2014D.cpp

---
+++ 2014D.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve()
+{
+  int n, d, k;
+  vector<int>vec(n+1);
+  
+  for(int i=0; i<k; i++)
+  {
+    int a,b;
+    cin>>a>>b;
+    a = max(1, a-d+1);
+    b = 
+
+  }
+  return 0;  
+}
+
+
+int main()
+{
+  int tc;
+  cin>>tc;
+  while(tc--)
+  {
+    solve();
+  }
+
+}
