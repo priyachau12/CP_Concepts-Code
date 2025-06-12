bfa166c5fb11d032a4d0e8c867af589f5d4c5381
priyachau12 <125198162+priyachau12@users.noreply.github.com>
2025-06-13 00:03:30 +0530

    Add 548448B.cpp

---
+++ 548448B.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve()
+{
+  int n;
+  cin>>n;
+  if(n%3==0)
+  {
+    cout<<"NO\n";
+  }
+  else
+  {
+    cout<<"YES\n";
+  }
+  return 0;  
+}
+
+
+int main()
+{
+  int tc;
+  cin>>tc;
+  while(tc--)
+  {
+    solve();
+  }
+
+}
