9e2ec436d8c595f536dcfdf38d48dfdc12ea75e4
priyachau12 <125198162+priyachau12@users.noreply.github.com>
2025-06-12 23:51:59 +0530

    Add 1659A.cpp

---
+++ 1659A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+int solve()
+{
+  int n, r, b;
+  cin>>n>>r>>b;
+  string res = "";
+  
+  cout<<res<<endl;
+  return 0;  
+}
+
+
+int32_t main()
+{
+  int tc;
+  cin>>tc;
+  while(tc--)
+  {
+    solve();
+  }
+
+}
