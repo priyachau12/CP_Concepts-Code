9593eaf14e8da54faa8011111a4eb7b341e98e81
priyachau12 <125198162+priyachau12@users.noreply.github.com>
2025-06-13 00:01:05 +0530

    Add 2093A.cpp

---
+++ 2093A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+int solve()
+{
+  int k;
+  cin>>k;
+  if(k%2)
+  {
+    cout<<"YES\n";
+  }
+  else
+  {
+    cout<<"NO\n";
+  }
+  return 0;  
+}
+
+
+int32_t main()
+{
+  int tc;
+  cin>>tc;
+  while(tc--)
+  {
+    solve();
+  }
+}
