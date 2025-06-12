81614b8aa13ec46efb0ec6cfde3eddb61c4d4449
priyachau12 <125198162+priyachau12@users.noreply.github.com>
2025-06-13 00:01:22 +0530

    Add 2106C.cpp

---
+++ 2106C.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+int solve()
+{
+  int n,k;
+  cin>>n>>k;
+  
+  vector<int> a(n), b(n);
+  for(int i=0; i<n; i++)
+  {
+    cin>>a[i];
+  }
+  for(int i=0; i<n; i++)
+  {
+    cin>>b[i];
+  }
+
+  return 0;  
+}
+
+
+int32_t main()
+{
+  int tc;
+  cin>>tc;
+  while(tc--)
+  {
+    solve();
+  }
+}
