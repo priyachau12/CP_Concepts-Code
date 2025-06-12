bf8ddd1f80b1d72021e98d00637097adb050366d
priyachau12 <125198162+priyachau12@users.noreply.github.com>
2025-06-13 00:03:10 +0530

    Add 547887B.cpp

---
+++ 547887B.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve()
+{
+  int n;
+  cin>>n;
+  string s;
+  cin>>s;
+  cout<<"The String is : "<<s<<endl;
+  return 0;  
+}
+
+
+int main()
+{
+  int tc=1;
+  //cin>>tc;
+  while(tc--)
+  {
+    solve();
+  }
+
+}
