d2abd3509dc0f2f531d04edda4a31171a8b06160
priyachau12 <125198162+priyachau12@users.noreply.github.com>
2025-06-13 00:03:51 +0530

    Add 590997E.cpp

---
+++ 590997E.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+int solve()
+{
+    int n, res=0, len=1, trans=0;
+    cin>>n;
+    string s;
+    cin>>s;
+    vector<int>dp(n);
+    
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        solve();
+    }
+}
+
